70feb952053e635d2c863ba0836a8d57269066ff
JJ OBrien <jj@jobrien9.com>
2020-11-29 22:08:59 -0500

    Potentiometer

--- KatelynTrain/src/KatelynTrain.cpp
+++ KatelynTrain/src/KatelynTrain.cpp
@@ -1,9 +1,3 @@
-/******************************************************/
-//       THIS IS A GENERATED FILE - DO NOT EDIT       //
-/******************************************************/
-
-#include "Particle.h"
-#line 1 "/home/jj/Documents/TrainControl/KatelynTrain/src/KatelynTrain.ino"
 /*
  * Project KatelynTrain
  * Description: Firmware to move the Christmas tree train
@@ -11,19 +5,19 @@
  * Date: 11/16/2020
  */
 
-void setup();
-void loop();
-#line 8 "/home/jj/Documents/TrainControl/KatelynTrain/src/KatelynTrain.ino"
-#define PWM_PIN D2
+#include "Particle.h"
+#ifndef KATELYN_TRAIN_H
+    #include <KatelynTrain.h>
+#endif
 
-int changeSpeed(String newSpeed);
-int killSwitch(String argsNotImplemented);
-
-
+//used to debounce potentiometer
+int lastPot = MIN_BITS_WITH_POT;
+int currentSpeed = 0;
 
 // setup() runs once, when the device is first turned on.
 void setup() {
   pinMode(PWM_PIN, OUTPUT);
+  pinMode(POT_PIN, INPUT);
 
   //register remote functions 
   Particle.function("changeSpeed", changeSpeed);
@@ -31,22 +25,50 @@ void setup() {
 }
 
 void loop() {
-
+  Serial.print("current speed: ");
+  Serial.println(currentSpeed);
+  //poll the potentiometer to see if we need to change speed
+  changeSpeedWithPotentiometer();
+  delay(2000);
 }
 
+//bounded from 1 to 50
 int changeSpeed(String newSpeed){
+  Serial.print("new speed: "); 
+  Serial.println(newSpeed);
   //255 is the most that the PWM can take
-  //the power supply goes to 19.22V, but the train can only take a max of 19.22V
-  //throw out anything above 12V (which is ~159)
+  //the power supply goes to 19.22V, but the train can only take a max of 12V
+  //the usable speeds seem to be between 150 (barely moves) and 200 (derails)
   int newSpeedAsInt = newSpeed.toInt();
-  // if (newSpeedAsInt > 150){
-  //   newSpeedAsInt = 150;
-  // }else if (newSpeedAsInt < 80)
-  // {
-  //   newSpeedAsInt = 0;
-  // }  
+  //PWM ranges from 0 to 255, but only 150-200 is useful to the train, so we work with those values
+  int pwmSignal = 0;
+  if (newSpeedAsInt > PWM_RANGE){
+    pwmSignal = MAX_PWM;
+  }else if (newSpeedAsInt > 0)
+  {
+    pwmSignal = MIN_PWM + newSpeedAsInt;
+  }else{
+    pwmSignal = 0;
+  }
 
-  analogWrite(PWM_PIN, newSpeedAsInt);
+  //todo: remove the currentspeed global variable once done debugging
+  currentSpeed = newSpeedAsInt;
+  analogWrite(PWM_PIN, pwmSignal);
+}
+
+void changeSpeedWithPotentiometer(){
+  int currentPot = analogRead(POT_PIN);
+  // Serial.print("current pot: ");
+  // Serial.println(currentPot);
+  // Serial.print("lastpot: ");
+  // Serial.println(lastPot);
+  int difference = lastPot - currentPot;
+
+  //debounce (i.e. no change less than 50 should be acknowledged)
+  if (difference > 50 || difference < -50) {
+    changeSpeed(String(((currentPot-MIN_BITS_WITH_POT)/(double)MIN_BITS_WITH_POT) * PWM_RANGE));
+    lastPot = currentPot;
+  }
 }
 
 int killSwitch(String argsNotImplemented){
---
+++ KatelynTrain/src/KatelynTrain.h
@@ -0,0 +1,19 @@
+
+#define PWM_PIN D2
+#define POT_PIN A0
+
+//max  MV that can be recorded on analog pins reading from 3V3 voltage source on Particle
+#define MAX_ADC_BITS 4096
+//usable PWM seems to be between 150-200 (update this if new data comes in, this was as of 11/29/2020)
+#define PWM_RANGE 50
+#define MAX_PWM 200
+#define MIN_PWM 150
+
+//the pot is paired with a 10K resistor on a voltage divider, so the lowest it will read is 1.65 V
+#define MIN_BITS_WITH_POT 2048
+
+int changeSpeed(String newSpeed);
+int killSwitch(String argsNotImplemented);
+void setup();
+void lool();
+void changeSpeedWithPotentiometer();
